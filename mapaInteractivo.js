ad70c9618e8b4ee07544dab55a6e01e9060efebf
AleMera <66742960+AleMera@users.noreply.github.com>
2020-06-11 21:38:33 -0500

    Update mapaInteractivo.js

--- mapaInteractivo.js
+++ mapaInteractivo.js
@@ -18,7 +18,9 @@ function onMapClick(e) {
     nuevoMarker = L.marker(e.latlng, {draggable: false});
     map.addLayer(nuevoMarker);
     nuevoMarker.bindPopup("Esta es tu ubicacion<br>" + e.latlng).openPopup();
-    document.getElementById('lati').value = e.latlng.lat;
-    document.getElementById('longi').value = e.latlng.lng;
+    var latPos = e.latlng.lat.toString();
+    var longPos = e.latlng.lng.toString();
+    document.getElementById('lati').value = latPos;
+    document.getElementById('longi').value = longPos;
 }
 map.on('click', onMapClick);
